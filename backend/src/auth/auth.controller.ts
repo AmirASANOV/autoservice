b9eee1adff29f460191bd9066e305436f5a3c3f3
Veronique Doss <vdoss@student.21-school.ru>
2024-02-20 20:20:56 +0300

    added swagger в контроллере auth и в одном dto

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -2,10 +2,12 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @Post('registration')
   registration(@Body() createUserDto: CreateUserDto) {
--- backend/src/auth/dto/create-user.dto.ts
+++ backend/src/auth/dto/create-user.dto.ts
@@ -1,15 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsPhoneNumber, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({ example: 'john', description: 'name' })
   @IsString()
   name: string;
 
+  @ApiProperty({ example: 'john@doe.com', description: 'email' })
   @IsEmail()
   email: string;
 
+  @ApiProperty({ example: 'pass ojnervoinro', description: 'pass' })
   @IsString()
   password: string;
 
+  @ApiProperty({ example: '+79991112233', description: 'phone' })
   @IsPhoneNumber()
   phoneNumber: string;
 }
